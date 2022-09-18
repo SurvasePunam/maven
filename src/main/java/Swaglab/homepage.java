64d4512ec0626997a4d8942985772f794f346a1a
SurvasePunam <survasepunam15@gmail.com>
2022-09-18 20:18:35 +0530

    delete trial2 method from swaglab

--- src/main/java/Swaglab/homepage.java
+++ src/main/java/Swaglab/homepage.java
@@ -5,7 +5,7 @@ public class homepage {
 	public static void main(String[] args) {
 		
 		System.out.println("homepage trial1 for swaglab");
-		System.out.println("homepage trial2 for swaglab on github");
+		
 		System.out.println("homepage trial3 for swaglab on github");
 	}
 
